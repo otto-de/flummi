5c151f988935f792d1c12dc476b0bd86254f1e39
Siarhei Hlasouski <shlasouski@exadel.com>
2017-05-19 17:16:49 +0300

    add support nested aggregations for Term aggregation

--- src/main/java/de/otto/flummi/aggregations/TermsBuilder.java
+++ src/main/java/de/otto/flummi/aggregations/TermsBuilder.java
@@ -1,15 +1,20 @@
 package de.otto.flummi.aggregations;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import de.otto.flummi.SortOrder;
 import de.otto.flummi.response.AggregationResult;
+import de.otto.flummi.response.Bucket;
+import de.otto.flummi.response.BucketAggregationResult;
 import de.otto.flummi.util.Pair;
 
 import java.util.ArrayList;
-
-import static de.otto.flummi.aggregations.AggregationResultParser.parseBuckets;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collector;
 
 public class TermsBuilder extends SubAggregationBuilder<TermsBuilder> {
     private String fieldName;
@@ -55,12 +60,50 @@ public class TermsBuilder extends SubAggregationBuilder<TermsBuilder> {
             orders.forEach(e -> orderObject.add(e.getKey(), new JsonPrimitive(e.getValue().toString())));
             fields.add("order", orderObject);
         }
+
+        if (subAggregations != null) {
+            JsonObject subAggregationsObject = subAggregations
+                    .stream()
+                    .collect(toJsonObject());
+
+            jsonObject.add("aggregations", subAggregationsObject);
+
+        }
         return jsonObject;
+    }
+
+    private static Collector<AggregationBuilder, JsonObject, JsonObject> toJsonObject() {
+        return Collector.of(JsonObject::new,
+                (json, a) -> json.add(a.getName(), a.build()),
+                (left, right) -> left);
     }
 
     @Override
     public AggregationResult parseResponse(JsonObject jsonObject) {
-        return parseBuckets(jsonObject);
+        AggregationResult aggregation = null;
+
+        JsonElement bucketsElement = jsonObject.get("buckets");
+        if (bucketsElement != null) {
+            JsonArray bucketsArray = bucketsElement.getAsJsonArray();
+            ArrayList<Bucket> bucketList = new ArrayList<>();
+            for (JsonElement elem : bucketsArray) {
+                JsonObject elemObject = elem.getAsJsonObject();
+
+                Map<String, AggregationResult> aggregations = new HashMap<>();
+
+                if (subAggregations != null) {
+                    subAggregations.stream().forEach(t ->
+                            aggregations.put(t.getName(), t.parseResponse(elemObject.get(t.getName()).getAsJsonObject())));
+                }
+
+                Bucket bucket = new Bucket(elemObject.get("key").getAsString(), elemObject.get("doc_count").getAsLong(), aggregations);
+
+                bucketList.add(bucket);
+
+            }
+            aggregation = new BucketAggregationResult(bucketList);
+        }
+        return aggregation;
     }
 
     public JsonElement buildValue() {
